760029cc67245808af10fac11a21778165563e0a
chiniu <kimpoychin131@gmail.com>
2026-06-30 17:05:31 +0800

    Add firebaseSetup

---
+++ firebase/app.js
@@ -0,0 +1,25 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getDatabase } from "firebase/database";
+import path from 'path';
+import dotenv from 'dotenv'
+
+const envpath = path.join(import.meta.dirname, "..", ".env");
+dotenv.config({path : envpath})
+
+const firebaseConfig = {
+  apiKey: process.env.APIKEY,
+  authDomain: "hanaplink.firebaseapp.com",
+  databaseURL: process.env.DB_URL,
+  projectId: "hanaplink",
+  storageBucket: "hanaplink.firebasestorage.app",
+  messagingSenderId: process.env.MESS_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEAS_ID
+};
+
+const app = initializeApp(firebaseConfig);
+const db  = getDatabase(app)
+
+console.log(app)
+export default db;
